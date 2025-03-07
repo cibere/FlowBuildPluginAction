6d5317d52a2180a30523cec7e747cd5f1c54192c
cibere <71997063+cibere@users.noreply.github.com>
2025-03-07 12:06:43 -0800

    Update update_version.py

--- update_version.py
+++ update_version.py
@@ -11,11 +11,20 @@ import json
 
 
 @click.command
-@click.argument("suffix")
-def main(suffix: str):
+@click.option("--suffix")
+@click.option("--final", is_flag=True)
+def main(suffix: str | None, final: bool):
     file = Path("plugin.json")
     data = json.loads(file.read_text())
-    data["Version"] = f"{data['Version']}-{suffix}"
+    current: str = data["Version"]
+
+    if suffix:
+        data["Version"] = f"{current}-{suffix}"
+    elif final:
+        for char in ("a", "b", "c"):
+            if char in current:
+                data['Version'] = current = current.split(char)[0]
+
     file.write_text(json.dumps(data, indent=4))
 
 
